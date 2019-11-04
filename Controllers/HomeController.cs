8fbe2681c3e325cf36c8b52fb6dff6fc31869dc7
Ihor <igstroch@gmail.com>
2019-11-04 22:50:36 +0200

    Homework done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,34 +26,43 @@ namespace MVCHomework.Controllers
             return View();
         }
 
+        // show all notes from json file
         public IActionResult GetAllNotes()
         {
             List<NoteVM> listOfNotes = JsonConvert.DeserializeObject<List<NoteVM>>(ReadJsonFile());
+
             listOfNotes = (from n in listOfNotes
                           orderby n.Id
                           select n).ToList();
+
             return View(listOfNotes);
         }
 
+        // go to the note creation form
         public IActionResult Create()
         {
             return View();
         }
 
+        // create note
         [HttpPost]
         public IActionResult Create(NoteVM model)
         {
             List<NoteVM> listOfNotes = JsonConvert.DeserializeObject<List<NoteVM>>(ReadJsonFile());
+
+            // add new note
             if (listOfNotes == null || listOfNotes.Count == 0)
                 model.Id = 1;
             else
                 model.Id = listOfNotes.Max(n => n.Id) + 1;
             model.DateTime = DateTime.Now;
             listOfNotes.Add(model);
+
             WriteJsonFile(JsonConvert.SerializeObject(listOfNotes));
             return RedirectToAction("GetAllNotes");
         }
 
+        // go to the note editing form
         public IActionResult Edit(int id)
         {
             List<NoteVM> listOfNotes = JsonConvert.DeserializeObject<List<NoteVM>>(ReadJsonFile());
@@ -63,41 +72,52 @@ namespace MVCHomework.Controllers
             return View(note);
         }
 
+        // edit note
         [HttpPost]
         public IActionResult Edit(NoteVM model)
         {
             List<NoteVM> listOfNotes = JsonConvert.DeserializeObject<List<NoteVM>>(ReadJsonFile());
+
+            // find note with needed id and replace it
             NoteVM note = (from n in listOfNotes
                            where n.Id == model.Id
                            select n).First();
             listOfNotes.Remove(note);
             model.DateTime = DateTime.Now;
             listOfNotes.Add(model);
+
             WriteJsonFile(JsonConvert.SerializeObject(listOfNotes));
             return RedirectToAction("GetAllNotes");
         }
 
+        // go to the note deletion page
         public IActionResult Delete(int id)
         {
             List<NoteVM> listOfNotes = JsonConvert.DeserializeObject<List<NoteVM>>(ReadJsonFile());
+
             NoteVM note = (from n in listOfNotes
                            where n.Id == id
                            select n).First();
             return View(note);
         }
 
+        // delete note
         [HttpPost]
         public IActionResult Delete(NoteVM model)
         {
             List<NoteVM> listOfNotes = JsonConvert.DeserializeObject<List<NoteVM>>(ReadJsonFile());
+
+            // find note with needed id and delete it
             NoteVM note = (from n in listOfNotes
                            where n.Id == model.Id
                            select n).First();
             listOfNotes.Remove(note);
+
             WriteJsonFile(JsonConvert.SerializeObject(listOfNotes));
             return RedirectToAction("GetAllNotes");
         }
 
+        // This method reads a json file and returns a string
         private string ReadJsonFile(string path = _path)
         {
             string jsonText;
@@ -108,6 +128,7 @@ namespace MVCHomework.Controllers
             return jsonText;
         }
 
+        // This method writes a string to a json file
         private void WriteJsonFile(string text, string path = _path)
         {
             using (StreamWriter sw = new StreamWriter(path))
